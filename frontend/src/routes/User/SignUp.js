0abfa470265a5aad4bf1a12707554bbacac0e5e2
woohyuk <wkdroal11@gmail.com>
2023-02-08 17:46:20 +0900

    Update SignUp.js
    비밀번호 불일치 돼도 가입되는 문제 해결

--- frontend/src/routes/User/SignUp.js
+++ frontend/src/routes/User/SignUp.js
@@ -60,7 +60,11 @@ const SignUp = () => {
     const onClickConfirmButton =() =>{
       if(email === User.email){
         alert('이미 등록된 이메일입니다.');
-      } else {
+      }
+      else if(pw!==pw2){
+        alert('비밀번호를 다시 확인해주세요.');
+      }
+      else {
         alert('회원가입 되셨습니다!.');
         navigate('/login');
       }

ec3fdbd3ebb52ea7508b79945c1901672bc3f19f
as1248 <jun76248@gmail.com>
2023-02-20 12:08:53 +0900

    Add GlobalStyle

---
+++ frontend/src/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: "Helvetica", "Arial", sans-serif;
+    color: #545b67;
+  }
+`;
+
+export default GlobalStyle;

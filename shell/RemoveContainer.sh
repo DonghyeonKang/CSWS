66b0cf7eb7b1c4fbc3481761fb3585110da71dd8
truemanburbank <juxtap.se@gmail.com>
2023-02-23 19:58:35 +0900

    Edit RemoveContainer.sh
    서버 authorized_key에 등록된 컨테이너 공개키를 삭제하는 내용 추가

--- shell/RemoveContainer.sh
+++ shell/RemoveContainer.sh
@@ -15,14 +15,24 @@ userDir="Users"
 # 컨테이너 삭제
 docker rm ${containerName}
 
+# 공개키 삭제
+cd ~/.ssh/
+pubKey=`cat ~/.ssh/${containerName}/${containerName}.pub`
+if grep -qxF "$pubKey" "authorized_keys"; then
+    remove=$(grep -nxF "$pubKey" "authorized_keys" | cut -d':' -f1)
+    sed -i "${remove}d" "authorized_keys"
+    echo "${containerName} 공개키 삭제"
+    rm -rf ~/.ssh/${containerName}
+fi
+
 # Users 디렉토리로 이동
-cd $userDir || exit
+cd ~/sh/$userDir || exit
 
 # 사용자 텍스트 파일에서 해당 컨테이너의 코드 삭제
 ##  유저 코드가 텍스트 파일에 존재하는지 확인
 if grep -qxF "$userCode" "$txtFile"; then
     remove=$(grep -nxF "$userCode" "$txtFile" | cut -d':' -f1)
-    sed -i "" "${remove}d" "$txtFile"
+    sed -i "${remove}d" "$txtFile"
     echo "${containerName} 삭제"
     exit 0
 fi

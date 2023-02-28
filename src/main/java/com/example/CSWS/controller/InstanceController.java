86aea296020417cf863bfe98fe1d5eb5592aae6a
DonghyeonKang <donghyeon3372@gmail.com>
2023-02-28 13:39:51 +0900

    Update InstanceController Structure

--- src/main/java/com/example/CSWS/controller/InstanceController.java
+++ src/main/java/com/example/CSWS/controller/InstanceController.java
@@ -2,7 +2,6 @@ package com.example.CSWS.controller;
 
 import com.example.CSWS.domain.Instance;
 import com.example.CSWS.domain.InstanceDto;
-import com.example.CSWS.common.shRunner.ShRunner;
 import com.example.CSWS.service.instance.InstanceCreateService;
 import com.example.CSWS.service.instance.InstanceManagementService;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,17 +23,18 @@ import java.util.Optional;
 // 서비스, 컨트롤러, 리포지토리를 연결한다.
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/instances")
 public class InstanceController {
 
     private final InstanceCreateService instanceCreateService;
     private final InstanceManagementService instanceManagementService;
 
-    @GetMapping("/instance/new")
+    @GetMapping("/new")
     public String createForm() {
         return "forTest/instances/createInstanceForm";
     }
 
-    @PostMapping("/instance/new")
+    @PostMapping("/new")
     public String create(InstanceDto instanceDto) {
 
         instanceCreateService.createInstance(instanceDto);
@@ -41,19 +42,51 @@ public class InstanceController {
         return "redirect:/";
     }
 
-    @GetMapping("/instances")
-    public String findList(Model model) { return "forTest/instances/findInstances"; }
+    // 인스턴스 목록 조회
+    @GetMapping()
+    public String getInstances(Model model) {
+        String username = "";
+        instanceManagementService.findInstanceList(username);
+        return "forTest/instances/findInstances";
+    }
 
-    @PostMapping("/instances")
-    public String list(Model model, String instanceId) {
+    // 인스턴스 세부사항 조회
+    @GetMapping("/details")
+    public String getInstanceDetails() {
+        String instanceId = "";
+        instanceManagementService.findInstanceDetails(instanceId);
+        return "details";
+    }
 
-        // 임시로 하나만 있는지 확인
-        Optional<InstanceDto> instance = instanceManagementService.findInstanceDetail(instanceId);
-        List<InstanceDto> instances = new ArrayList<>();
-        
-        // 객체가 존재할 때만 리스트에 추가
-        instance.ifPresent(instances::add);
-        model.addAttribute("instances", instances);
-        return "forTest/instances/instanceList";
+    // 인스턴스 시작
+    @PostMapping("/start")
+    public String startInstance() {
+        String instanceId = "";
+        instanceManagementService.startInstance(instanceId);
+        return "start";
+    }
+
+    // 인스턴스 재시작
+    @PostMapping("/restart")
+    public String restartInstance() {
+        String instanceId = "";
+        instanceManagementService.restartInstance(instanceId);
+        return "restart";
+    }
+
+    // 인스턴스 정지
+    @PostMapping("/stop")
+    public String stopInstance() {
+        String instanceId = "";
+        instanceManagementService.stopInstance(instanceId);
+        return "stop";
+    }
+
+    // 인스턴스 삭제
+    @PostMapping("/delete")
+    public String deleteInstance() {
+        String instanceId = "";
+        instanceManagementService.deleteInstance(instanceId);
+        return "delete";
     }
 }

938dd438c493fefc753d0dc2b3c1d57712102c8a
as1248 <jun76248@gmail.com>
2023-03-08 17:25:15 +0900

    Simplify EditInboundRules
    인바운드 규칙 페이지에서 이미 존재하는 규칙과 새로 추가한 규칙에 대해 컴포넌트로 분리함

--- frontend/src/components/Instance/InboundRules/EditInboundRules.js
+++ frontend/src/components/Instance/InboundRules/EditInboundRules.js
@@ -1,30 +1,34 @@
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
+import NewInboundRule from "./NewInboundRule";
+import ExistingInboundRule from "./ExistingInboundRule";
 
 const EditInboundRules = () => {
     const navigate = useNavigate();
     const {instanceId,securityGroupId} = useParams();
-    const [number, setNumber] = useState(1);
+    const [id, setId] = useState(1);
     const [data,setData] = useState([{
-        Id:1,
-        type:2,
-        protocol:3,
-        port:4,
-        CIDR:5,
-        number:0,
+        Id:'0',
+        type:'2',
+        protocol:'3',
+        port:'4',
+        CIDR:'5',
     }]);
+    const [newData, setNewData] = useState([]);
     const addData = () => {
-        setNumber((prev)=>prev+1);
-        setData([...data,{
-        Id:1,
-        type:2,
-        protocol:3,
-        port:4,
-        CIDR:5,
-        number
+        setNewData((prev) => [...prev,{
+        Id:id,
+        type:'2',
+        protocol:'3',
+        port:'4',
+        CIDR:'5',
         }]);
-    }
+        setId((prev)=>prev+1);
+    };
+    const saveRules = () => {
+      console.log('새로운 규칙은 POST요청, 삭제할 규칙은 DELETE요청');
+    };
     return (
         <>
           <Title>인바운드 규칙 편집</Title>
@@ -35,26 +39,16 @@ const EditInboundRules = () => {
                     <RulesHeader style={{width:'12vw', minWidth:'150px'}}>보안 그룹 ID</RulesHeader>
                     <RulesHeader style={{width:'12vw', minWidth:'150px'}}>유형</RulesHeader>
                     <RulesHeader style={{width:'5vw', minWidth:'80px'}}>프로토콜</RulesHeader>
-                    <RulesHeader style={{width:'5vw', minWidth:'80px'}}>포트 범위</RulesHeader>
+                    <RulesHeader style={{width:'6vw', minWidth:'100px'}}>포트 범위</RulesHeader>
                     <RulesHeader style={{width:'12vw', minWidth:'150px'}}>CIDR 블록</RulesHeader>
                     <th> </th>
                 </tr>
                 {data.map((i)=>{
-                    return(
-                    <tr>
-                      <td style={{width:'12vw', minWidth:'150px'}}>{i.Id}</td>
-                      <td><select style={{width:'12vw', minWidth:'150px'}}>
-                        <option>{i.type}</option>
-                      </select></td>
-                      <td><input style={{width:'5vw', minWidth:'80px'}} value={i.protocol}/></td>
-                      <td><input style={{width:'5vw', minWidth:'80px'}} value={i.port}/></td>
-                      <td><select style={{width:'12vw', minWidth:'150px'}}>
-                        <option>{i.CIDR}</option>
-                      </select></td>
-                      <DeleteRule onClick={()=>{setData(data.filter((item)=>item.number !== i.number))}}>
-                        삭제
-                      </DeleteRule>
-                    </tr>)})}
+                    return(<ExistingInboundRule data={data} setData={setData} i={i}/>)})
+                }
+                {newData.map((i)=>{
+                    return(<NewInboundRule data={newData} setData={setNewData} i={i}/>)})
+                }
                 </tbody>
                 </Rules>
             </Container>
@@ -62,7 +56,7 @@ const EditInboundRules = () => {
             <AddRule onClick={()=>addData()}>규칙 추가</AddRule>
             <div>
                 <Cancel onClick={() => navigate(`/dashboard/${instanceId}/${securityGroupId}`)}>취소</Cancel>
-                <SaveRules onClick={() => navigate(`/dashboard/${instanceId}/${securityGroupId}`)}>인바운드 규칙 저장</SaveRules>
+                <SaveRules onClick={saveRules}>인바운드 규칙 저장</SaveRules>
             </div>
           </BtnSection>
         </>
@@ -91,18 +85,6 @@ const Rules = styled.table`
 const RulesHeader = styled.th`
   text-align: left;
   margin-right: 5vw;
-`;
-
-const DeleteRule = styled.td`
-  cursor: pointer;
-  background-color: white;
-  border: 1px solid black;
-  text-align: center;
-  width: 50px;
-  &:hover{
-    background-color: #fafafa;
-    color: black;
-  }
 `;
 
 const BtnSection = styled.div`
